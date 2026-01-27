fec140608281c5b89486fd4de42f690e7005c2f8
AlxMoreno <alexandremoreno.loja@gmail.com>
2026-01-27 17:56:07 -0400

    Versão com arquivo env

--- dataset.py
+++ dataset.py
@@ -5,9 +5,33 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import os
+from dotenv import load_dotenv
+import json
 
 # fazendo autenticação com as credenciais do usuario google
-gc = gspread.service_account(filename="c:\\python\\credenciais.json")
+#gc = gspread.service_account(filename="c:\\python\\credenciais.json")
+
+load_dotenv()
+
+credenciais = {
+  "type": os.getenv("TYPE"),
+  "project_id": os.getenv("PROJECT_ID"),
+  "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+  "private_key": os.getenv("PRIVATE_KEY"),
+  "client_email": os.getenv("CLIENT_EMAIL"),
+  "client_id": os.getenv("CLIENT_ID"),
+  "auth_uri": os.getenv("AUTH_URI"),
+  "token_uri": os.getenv("TOKEN_URI"),
+  "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+  "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+  "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
+}
+
+#credencialgoogle = json.dumps(os.getenv("api_google"))
+#teste = json.dumps(getenv("api_google"), ident=4)
+print(credenciais)
+
+gc = gspread.service_account_from_dict(credenciais)
 
 # pegando os dados da planilha online
 dados = gc.open_by_url("https://docs.google.com/spreadsheets/d/1Yqb3QzcyV026GiwG75gw50_bMr0MV6LjNeFv10F2s44/edit?gid=0#gid=0").worksheet("lancamentos")
