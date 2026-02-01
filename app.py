9ba287393bc39b480f3c91dbcae40fb5f7b1d187
AlxMoreno <alexandremoreno.loja@gmail.com>
2026-01-31 22:26:30 -0400

    Versao com agrupamento

--- app.py
+++ app.py
@@ -7,25 +7,34 @@ from dataset import planilhaloft2
 st.set_page_config(layout='wide')
 st.title("Dashboard Grupo Ideal")
 
+# agrupando totais por departamento
+departamentosContainer = planilhacontainer.groupby("Tipo")["Total"].sum().astype(float)
+departamentosLoft1 = planilhaloft1.groupby("Tipo")["Total"].sum().astype(float)
+departamentosLoft2 = planilhaloft2.groupby("Tipo")["Total"].sum().astype(float)
+
 aba1, aba2, aba3 = st.tabs(['Hotel Container', 'Loft 1', 'Loft 2'])
 
 #https://www.udemy.com/course/desenvolvendo-dashboards-em-python/learn/lecture/38681324#overview  /etapa do curso
 with aba1:
     st.dataframe(planilhacontainer)
+    st.dataframe(departamentosContainer)
 
     coluna1, coluna2 = st.columns(2)
     with coluna1:
         totalcontainer = f"R$ {planilhacontainer['Total'].sum():,.2f}".replace(",", "v").replace(".", ",").replace("v", ".")
-        st.metric('Total Gasto:', totalcontainer)  
+        st.metric('Total Gasto:', totalcontainer)
+
 with aba2:
     st.dataframe(planilhaloft1)
-
+    st.dataframe(departamentosLoft1)
+    
     coluna21, coluna22 = st.columns(2)
     with coluna21:
         totalloft1 = f"R$ {planilhaloft1['Total'].sum():,.2f}".replace(",", "v").replace(".", ",").replace("v", ".")
         st.metric('Total Gasto:', totalloft1) 
 with aba3:
     st.dataframe(planilhaloft2)
+    st.dataframe(departamentosLoft2)
 
     coluna31, coluna32 = st.columns(2)
     with coluna31:
@@ -33,8 +42,7 @@ with aba3:
         st.metric('Total Gasto:', totalloft2)     
       
 
-# agrupando totais por departamento
-#departamentos = planilhacontainer.groupby("Tipo")["Total"].sum().astype(float)
+
 
 #print(departamentos)
 
